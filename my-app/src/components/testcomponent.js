43fce4623d5e0cc4d350914b5f391205fd094ae6
dEEGE2time <johannesweidensjo@hotmail.com>
2023-07-21 16:21:46 +0000

    install ES7 snippets extension

---
+++ my-app/src/components/testcomponent.js
@@ -0,0 +1,71 @@
+// "rfce"
+
+// import React from 'react'
+
+// function testcomponent() {
+//   return (
+//     <div>testcomponent</div>
+//   )
+// }
+
+// export default testcomponent
+// ----------------------------------------------------------
+
+
+
+
+
+// "rafce", arrow functions
+
+// import React from 'react'
+
+// const testcomponent = () => {
+//   return (
+//     <div>testcomponent</div>
+//   )
+// }
+
+// export default testcomponent
+// ----------------------------------------------------------
+
+
+
+
+
+// "rce", class components
+
+// import React, { Component } from 'react'
+
+// export class testcomponent extends Component {
+//   render() {
+//     return (
+//       <div>testcomponent</div>
+//     )
+//   }
+// }
+
+// export default testcomponent
+// ----------------------------------------------------------
+
+
+
+
+
+// "rcon" create constructor
+
+// constructor(props) {
+//   super(props)
+
+//   this.state = {
+     
+//   }
+// }
+// ----------------------------------------------------------
+
+
+
+
+
+// "clo" log object along name as string
+
+// console.log('first', first)
